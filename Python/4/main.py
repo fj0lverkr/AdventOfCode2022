1ba79d14806f26181b319aa0d7cfaf1362283aae
nils <nils.nahooy@live.com>
2022-12-12 16:11:49 +0100

    day 4 part 2

--- Python/4/main.py
+++ Python/4/main.py
@@ -1,5 +1,6 @@
-def part_one(f) -> int:
-    result = 0
+def both_parts(f) -> list[int]:
+    containing = 0
+    overlapping = 0
     with open(f, 'r') as input:
         pairs = input.readlines()
         for pair in pairs:
@@ -13,10 +14,13 @@ def part_one(f) -> int:
             elf_two_start = int(elf_two_split[0])
             elf_one_end = int(elf_one_split[1])
             elf_two_end = int(elf_two_split[1])
-            if (elf_one_start <= elf_two_start and elf_one_end >= elf_two_end) or (elf_two_start <= elf_one_start and elf_two_end >= elf_one_end):
-                result += 1
-    return result
+            if (elf_one_start <= elf_two_start and elf_one_end >= elf_two_end) or \
+                    (elf_two_start <= elf_one_start and elf_two_end >= elf_one_end):
+                containing += 1
+            if max(elf_one_start, elf_two_start) <= min(elf_one_end, elf_two_end):
+                overlapping += 1
+    return [containing, overlapping]
 
 
 if __name__ == "__main__":
-    print(part_one('Python/4/input.txt'))
+    print(both_parts('Python/4/input.txt'))
